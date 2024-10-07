4dcea1a42a46f1e16f314c49ae5d368082b5dd34
Triston Armstrong <tristonarmstrong@Tristons-MacBook-Pro.local>
2024-10-07 00:52:12 -0400

    add ability to export and import to other browsers

---
+++ src/components/cardSelector/ImportButton.tsx
@@ -0,0 +1,64 @@
+import images from "../../signals/images"
+import notes from "../../signals/notes"
+import { Card } from "../../types"
+import { convertBase64ToJson } from "../../utils/convertBase64ToJson"
+import { updateLocalStorage } from "../../utils/localStorage"
+import { defaultClassName } from "./utils"
+
+export function ImportButton() {
+
+  function _handleImport() {
+    const input = document.createElement('input')
+    input.type = 'file'
+    input.onchange = (e: any) => {
+      const file = e.target.files[0]
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
+      reader.onload = function(readerEvent) {
+        let content = readerEvent.target?.result;
+        // get only the base64 parts and not any identifiers
+        content = (content as string).split(',')[1]
+        const data: Record<string, Card<'note'> | Card<'image'>> = convertBase64ToJson(content)
+        for (let key in data) {
+          const item = data[key]
+          if (item.type == 'image') {
+            const { id, ...rest } = item
+            images.addImage(rest)
+          }
+
+          if (item.type == 'note') {
+            const { id, ...rest } = item
+            notes.addNote(rest)
+          }
+        }
+
+        updateLocalStorage('notes', notes.notes.value)
+        updateLocalStorage('images', images.images.value)
+        notes.notes.notify()
+        images.images.notify()
+      }
+    }
+    input.click()
+  }
+
+  return (
+    <svg
+      onclick={_handleImport}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className={"rotate-[180deg] " + defaultClassName}
+    >
+      <path
+        d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+      <polyline points="7 10 12 15 17 10" />
+      <line x1="12" x2="12" y1="15" y2="3" />
+    </svg>
+  )
+}
